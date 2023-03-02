6ec5f347c8e6df40d3f75253ee9226895ad43a5a
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-03-02 11:34:42 +0100

    Kommentarer og print

--- resources.py
+++ resources.py
@@ -1,2 +1,10 @@
 for i in range(5):
     print(i, end='suffiks ')
+
+print("\n")
+
+# .format:
+name = "Alice"
+age = 25
+message = "My name is {}, and I am {} years old".format(name, age)
+print(message)
--- task2_Client.py
+++ task2_Client.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 
-if len(sys.argv) < 4:       # We need 4 arguments that the client writes in the terminal
+if len(sys.argv) < 4:       # De 4 argumentene som klienten skriver i terminalen
     print("Usage: client.py server_host server_port filename")
     sys.exit()
 
@@ -9,21 +9,28 @@ server_host = sys.argv[1]
 server_port = int(sys.argv[2])
 filename = sys.argv[3]
 
+print("server_host:", server_host)
+print("server_port:", server_port)
+print("filename:", filename)
+
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("clientSocket:", clientSocket)
 clientSocket.connect((server_host, server_port))
 
 http_request = "GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n".format(filename, server_host)
-# Will look like this:
+# http_request:
 # GET /index.html HTTP/1.1
 # Host: 127.0.0.1
 
 clientSocket.send(http_request.encode())
-# Sends an HTTP request to the server
+# Sender http request-en til serveren
 
 response = clientSocket.recv(1024)
-# Receives the server's response
+# Mottar serverens response
+print("response:", response)                        # Mottar headeren
+print("response decoded:", response.decode())
 
-while response:
+while response:                                     # Mottar (.recv) 1 gang for hver sending (.send)
     print(response.decode(), end='')
     response = clientSocket.recv(1024)
 
