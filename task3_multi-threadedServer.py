0e70927ca3fb262ae544f0c7e39b2115222a35c8
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-03-02 14:21:32 +0100

    I mål med task3 minus kommentarer

--- task3_multi-threadedServer.py
+++ task3_multi-threadedServer.py
@@ -6,13 +6,13 @@ import _thread as thread
 def listen_for_clients(connection):
     while True:
         connectionSocket, addr = connection.accept()
-        thread.start_new_thread(handle_requests, (connectionSocket, addr))
+        thread.start_new_thread(handle_requests, (connectionSocket,))   # komma -> tuple med ett element
 
 
-def handle_requests(connection):
+def handle_requests(connectionSocket):
     while True:
         try:
-            message = connection.recv(1024).decode()
+            message = connectionSocket.recv(1024).decode()
             filename = message.split()[1]
             filename = filename[1:]
             f = open(filename)
@@ -21,17 +21,17 @@ def handle_requests(connection):
 
             header = 'HTTP/1.1 200 OK\r\n'
             header += 'Content-Type: text/html\r\n\r\n'
-            connection.send(header.encode())
+            connectionSocket.send(header.encode())
 
             for i in range(0, len(outputdata)):
-                connection.send(outputdata[i].encode())
-            connection.send("\r\n".encode())
-            connection.close()
+                connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
             break
         except IOError:
-            connection.send("HTTP/2.2 404 Not found\r\n\r\n <html> Feil side!  </html>".encode())
-            connection.close()
-            continue
+            connectionSocket.send("HTTP/2.2 404 Not found\r\n\r\n <html> Feil side!  </html>".encode())
+            connectionSocket.close()
+            break
 
 
 def main():
@@ -44,7 +44,7 @@ def main():
     serverSocket.listen(1)
     listen_for_clients(serverSocket)
     serverSocket.close()
-    sys.exit()
+
 
 if __name__ == '__main__':
     main()
